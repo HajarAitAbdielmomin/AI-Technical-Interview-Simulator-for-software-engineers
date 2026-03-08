dcbec91b58fc677b02c95f45cd9f0bbd37d34b84
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-03-08 23:37:33 +0000

    add envs and new service for interviews

--- Frontend/src/app/auth.service.ts
+++ Frontend/src/app/auth.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, tap} from 'rxjs';
 import {StorageService} from './storage.service';
+import {environment} from '../environments/environment.development';
 
-const baseUrl = 'http://localhost:8989/api';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -17,16 +17,16 @@ export class AuthService {
   constructor(private http: HttpClient, private storageService: StorageService) {}
 
   authenticateUser(email: any, password: any): Observable<any> {
-    const url = `${baseUrl}/users/auth/signin`;
+    const url = `${environment.apiUrl}/users/auth/signin`;
     return this.http.post(url, { email, password }, httpOptions);
   }
   createUser(username: any,email: any, password: any):Observable<any> {
-    const url = `${baseUrl}/users/auth/signup`;
+    const url = `${environment.apiUrl}/users/auth/signup`;
     return this.http.post(url, { username, email, password }, { ...httpOptions, responseType: 'text' });
   }
 
   logout():Observable<any>{
-    const url = `${baseUrl}/auth/logout`;
+    const url = `${environment.apiUrl}/auth/logout`;
     return this.http.post(url, {}).pipe(
       tap(() => {
         this.storageService.logout();
---
+++ Frontend/src/app/interview.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {environment} from '../environments/environment.development';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InterviewService {
+
+  constructor(private http: HttpClient) {}
+
+  getInterviews(): Observable<any> {
+    const url = `${environment.apiUrl}/interviews`;
+    return this.http.get(url, httpOptions);
+  }
+
+  getInterviewById(id: string): Observable<any> {
+    const url = `${environment.apiUrl}/interviews/${id}`;
+    return this.http.get(url, httpOptions);
+  }
+
+  createInterview(data: any): Observable<any> {
+    const url = `${environment.apiUrl}/interviews`;
+    return this.http.post(url, data, httpOptions);
+  }
+
+
+  submitAnswer(interviewId: string, questionId: string, answer: any): Observable<any> {
+    const url = `${environment.apiUrl}/interviews/${interviewId}/answers`;
+    return this.http.post(url, { questionId, answer }, httpOptions);
+  }
+}
