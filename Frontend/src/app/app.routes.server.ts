df3a0d03888f32e72f8858d5f471d3103d547a79
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2026-03-15 21:38:38 +0000

    Disable prerender for that route

--- Frontend/src/app/app.routes.server.ts
+++ Frontend/src/app/app.routes.server.ts
@@ -1,6 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // Dynamic routes → must be Client rendered (runtime data)
+  { path: 'user/interview/:id',        renderMode: RenderMode.Client },
+  { path: 'user/interview/:id/resume', renderMode: RenderMode.Client },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender
